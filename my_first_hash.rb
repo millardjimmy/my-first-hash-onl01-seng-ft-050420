9edee6d748a90bd0829999abfafc4116f2feb478
Jimmy Millard <millardjimmy@gmail.com>
2020-05-10 00:07:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+my_hash = {"name" => "Jimmy Millard", "age" => 29, "location" => "Montana"}
   
 end
 
